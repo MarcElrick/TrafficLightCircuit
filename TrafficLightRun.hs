4ec02b1268869bc49d98570ac14096340728c5ca
Ethan <2387502i@student.gla.ac.uk>
2021-11-11 12:46:16 +0000

    Added test case to driver, status report close to done

--- TrafficLightRun.hs
+++ TrafficLightRun.hs
@@ -1,56 +1,85 @@
--- Mux1Run: simulation driver for mux1 circuit
--- This file is part of Hydra.  John O'Donnell, 2021
+-- Driver Circuit
 
 module Main where
 import HDL.Hydra.Core.Lib
 import TrafficLight
 
-test_data_1 :: [String]
-test_data_1 =
+--Declaration of all test data sets
+test_data_v1_1, test_data_v1_2 :: [String]
+
+test_data_v1_1 =
 --------------
---   reset  --
+--   Normal use (reset to begin sequence only)  --
 --------------
-  [ "1"  --  
-  , "0"  --  
-  , "0"  --  
-  , "0"  --  
-  , "0"  --  
-  , "0"  --  
-  , "0"  --  
-  , "0"  --  
-  , "0"  --  
-  , "0"  --  
-  , "0"  --  reset, cycle 11
-  , "0"  --  
-  , "0"  --  
-  , "0"  -- 
-  , "0"  --    , "0"  --  
-  , "0"  --  
-  , "0"  --  
-  , "0"  --  reset, cycle 11
-  , "0"  --  
-  , "0"  --  
-  , "0"  -- 
-  , "0"  --  
+  [ "1"  --
+  , "0"  --
+  , "0"  --
+  , "0"  --
+  , "0"  --
+  , "0"  --
+  , "0"  --
+  , "0"  --
+  , "0"  --
+  , "0"  --
+  , "0"  --
+  , "0"  --
+  , "0"  --
+  , "0"  --
+  , "0"  --
+  , "0"  --
+  , "0"  --
+  , "0"  --
+  , "0"  --
+  , "0"  --
+  , "0"  --
+  , "0"  --
   ]
+
+test_data_v1_2 =
+  --------------
+  --   Reset pressed during sequence  --
+  --------------
+    [ "1"  --
+    , "0"  --
+    , "0"  --
+    , "0"  --
+    , "0"  --
+    , "0"  --
+    , "0"  --
+    , "0"  --
+    , "0"  --
+    , "0"  --
+    , "1"  --
+    , "0"  --
+    , "0"  --
+    , "0"  --
+    , "0"  --
+    , "0"  --
+    , "0"  --
+    , "0"  --
+    , "0"  --
+    , "0"  --
+    , "0"  --
+    , "0"  --
+    ]
 
 main :: IO ()
 main = driver $ do
 
--- Input data
-  useData test_data_1
+-- Input data. Specifies what test data set we use
+  useData test_data_v1_1
 
--- Input ports  
+-- Input ports (we only have a reset input)
   in_reset <- inPortBit "reset"
-
 
 -- Input signals
   let reset = inbsig in_reset
 
--- Circuit
+-- Circuit, with outputs red, amber and green. As it is a state machine only one
+-- of these will be '1' at one time.
   let (red, amber, green) = controller1 reset
 
--- Format the results  
+-- Format the outputs for each cycle
   format
     [string "  reset=", bit reset,
      string "  output red=", bit red,
