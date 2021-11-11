895afca1153443066e2feffec4c5e29909a2cff4
AbubakrMahmood <abubakrmahmood@hotmail.co.uk>
2021-11-11 09:45:05 +0000

    Finished main light transition functionality for v2

--- TrafficLight.hs
+++ TrafficLight.hs
@@ -1,7 +1,10 @@
 module TrafficLight where
-import HDL.Hydra.Core.Lib (CBit(..), Logic(inv, or4, and2, or2, or3))
+import HDL.Hydra.Core.Lib (CBit(..), Logic(inv, or4, and2, or2, or3, and3))
 import HDL.Hydra.Circuits.Combinational ()
 
+------------------------------------------------------------------------
+-- Simulate controller1 circuit
+------------------------------------------------------------------------
 
  -- Traffic light circuit v1
  -- Input: 1 bit word representing reset
@@ -15,6 +18,7 @@ controller1 reset = (red, amber, green)
           red = or4 x4 x5 x6 x7 
           reset' = inv reset
 
+          -- dffs numbered in order of light transition on clock tick
           x0 = dff (or2 reset x8)
           x1 = dff (and2 reset' x0)
           x2 = dff (and2 reset' x1)
@@ -24,3 +28,32 @@ controller1 reset = (red, amber, green)
           x6 = dff (and2 reset' x5)
           x7 = dff (and2 reset' x6)
           x8 = dff (and2 reset' x7)
+
+------------------------------------------------------------------------
+-- Simulate controller2 circuit
+------------------------------------------------------------------------
+
+controller2 :: CBit a => a -> a -> (a, a, a, a, a)
+controller2 reset walkRequest = (red, amber, green, wait, walk)
+    where green = and3 idleState (inv red) (inv amber)
+          amber = or2 x0 x4
+          red = or3 x1 x2 x3
+          wait = or2 green amber
+          walk = red
+
+          --walkPressed = and3 reset' walkRequest (inv (and2 countBuffer idleState))
+          --countBuffer = dff (and2 reset' walkPressed)
+
+          reset' = inv reset
+
+          -- determines if walk button is pressed and to ignore multiple presses in a cycle
+          idleState = dff (inv cycleOccuring) 
+          cycleOccuring = and3 reset' buttonPressBuffer (inv x4) 
+          buttonPressBuffer = and3 reset' walkRequest (inv x0)
+
+          -- dffs numbered in order of light transition on clock tick
+          x0 = dff buttonPressBuffer
+          x1 = dff (and2 reset' x0)
+          x2 = dff (and2 reset' x1)
+          x3 = dff (and2 reset' x2)
+          x4 = dff (and2 reset' x3)
--- TrafficLightRun.hs
+++ TrafficLightRun.hs
@@ -3,16 +3,32 @@
 
 module Main where
 import HDL.Hydra.Core.Lib
-import TrafficLight
+import TrafficLight ( controller1, controller2 )
 
-test_data_1 :: [String]
-test_data_1 =
+
+------------------------------------------------------------------------
+-- Running executable functions of type IO ()
+------------------------------------------------------------------------
+
+main :: IO ()
+main = do
+  putStrLn "*** controller1 simulation ***"
+  controller1Driver
+  putStrLn "*** controller2 simulation ***"
+  controller2Driver
+
+------------------------------------------------------------------------
+-- Simulate controller1 circuit
+------------------------------------------------------------------------
+
+controller1TestData :: [String]
+controller1TestData =
 --------------
 --   reset  --
 --------------
-  [ "1"  --  
+  [ "0"  --  
   , "0"  --  
-  , "0"  --  
+  , "1"  --  
   , "0"  --  
   , "0"  --  
   , "0"  --  
@@ -28,11 +44,11 @@ test_data_1 =
   , "0"  -- 
   ]
 
-main :: IO ()
-main = driver $ do
+controller1Driver :: IO ()
+controller1Driver = driver $ do
 
 -- Input data
-  useData test_data_1
+  useData controller1TestData
 
 -- Input ports  
   in_reset <- inPortBit "reset"
@@ -50,6 +66,67 @@ main = driver $ do
      string "  output red=", bit red,
      string "  output amber=", bit amber,
      string "  output green=", bit green
+    ]
+
+-- Run the circuit on the test data
+  runSimulation
+
+
+------------------------------------------------------------------------
+-- Simulate controller2 circuit
+------------------------------------------------------------------------
+
+controller2TestData :: [String]
+controller2TestData =
+------------------------
+--  reset  walkRequest 
+------------------------
+  [ "0  0"  --  
+  , "0  0"  --  
+  , "0  0"  --  
+  , "0  1"  --  
+  , "0  0"  --  
+  , "0  0"  --  
+  , "0  0"  --  
+  , "0  1"  --  
+  , "0  0"  --  
+  , "0  0"  --  
+  , "1  0"  --  reset, cycle 11
+  , "0  0"  --  
+  , "0  0"  --  
+  , "0  0"  -- 
+  , "0  0"  --  
+  , "0  0"  -- 
+  ]
+
+controller2Driver :: IO ()
+controller2Driver = driver $ do
+
+-- Input data
+  useData controller2TestData
+
+-- Input ports  
+  in_reset <- inPortBit "reset"
+  in_walkRequest <- inPortBit "walkRequest"
+
+
+-- Input signals
+  let reset = inbsig in_reset
+  let walkRequest = inbsig in_walkRequest
+
+-- Circuit
+  let (red, amber, green, wait, walk) = controller2 reset walkRequest
+
+-- Format the results  
+  format
+    [string "  reset=", bit reset,
+     string "  walkRequest=", bit walkRequest,
+     string "  output red=", bit red,
+     string "  output amber=", bit amber,
+     string "  output green=", bit green,
+     string "  output wait=", bit wait,
+     string "  output walk=", bit walk
+
     ]
 
 -- Run the circuit on the test data
